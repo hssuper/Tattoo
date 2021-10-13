2a20557c64de9183a5c6614e33eebd0b9e5f976a
hssuper <higorsouza2306@gmail.com>
2021-10-13 11:13:53 -0300

    add

--- tatuagens.php
+++ tatuagens.php
@@ -3,7 +3,7 @@ include_once "include/menuadm.php";
 include_once 'C:/xampp/htdocs/tattoo/controller/agendamentoController.php';
 include_once 'C:/xampp/htdocs/tattoo/model/agendamento.php';
 include_once 'C:/xampp/htdocs/tattoo/model/mensagem.php';
-
+include_once 'C:/xampp/htdocs/tattoo/upload.php';
 $msg = new mensagem();
 $ag = new Agendamento();
 $btEnviar = FALSE;
