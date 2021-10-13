d3f2f358ea0fa945d0e00ef1cbd6fd7462c55205
hssuper <higorsouza2306@gmail.com>
2021-10-13 11:02:39 -0300

    add

--- upload.php
+++ upload.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+    
+
 <?php
 require_once 'C:/xampp/htdocs/tattoo/bd/bd.php';
 $imagem = $_FILES['imagem']['imagem'];
@@ -20,7 +26,7 @@ if($_FILES['imagem']['error'] != 0){
 
 $extensao = strtolower(end(explode('.', $_FILES['imagem'])));
 if(array_search($extensao, $_UP['extensoes']) === false){
-    $query = mysqli_query($conn,"INSERT INTO carousels){
+    $query = mysqli_query($conn,"INSERT INTO orcamento){
         imagem,
         created)VALUES('imagem',NOW())");
         echo "<META HTTP-EQUIV-REFRESH CONTENT = '0;URL=http://localhost/tattoo/tatuagens.php'>
@@ -30,5 +36,32 @@ if(array_search($extensao, $_UP['extensoes']) === false){
         </script>
         ";
     }
+    else if($_UP['tamanho'] < $_FILES['imagem']['size'] ){
+        echo "<META HTTP-EQUIV-REFRESH CONTENT = '0;URL=http://localhost/tattoo/tatuagens.php'>
+        <script type=\"text/javascript\">
+        alert(\"Arquivo muito grande.\");
+        </script>
+        ";
+    }
+    else{
+        if($_UP['renomeia'] == true){
+            $imagem_final = time().'.jpg';
+        }else{
+            $imagem_final = $_FILES['imagem']['imagem'];
+        }
+        if(move_uploaded_file($_FILES['imagem']['tmp_imagem'],$_UP['imagens']. $imagem_final )){
+            $query = mysqli_query($conn,"INSERT INTO orcamento){
+                imagem,
+                created)VALUES('imagem_final',NOW())");
+                echo "<META HTTP-EQUIV-REFRESH CONTENT = '0;URL=http://localhost/tattoo/tatuagens.php'>
+                <script type=\"text/javascript\">
+                alert(\"A imagem foi cadastrada.\");
+                </script>
+                ";
+        }
+    }
 
 ?>
+</head>
+</body>
+</html>
