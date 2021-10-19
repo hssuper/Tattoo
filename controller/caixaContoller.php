aba8c36160da84a0b1e51ef071a5a714ba366b3d
hssuper <higorsouza2306@gmail.com>
2021-10-19 09:34:40 -0300

    add

---
+++ controller/caixaContoller.php
@@ -0,0 +1,29 @@
+<?php
+include_once 'C:/xampp/htdocs/tattoo/dao/DaoCaixa.php';
+include_once 'C:/xampp/htdocs/tattoo/model/caixa.php';
+
+class caixaController
+{
+
+    public function inserirCaixa($NrPar, $dtPag, $frPag)
+    {
+        $caixa = new Caixa();
+        $caixa->setNrPar($NrPar);
+        $caixa->setDtPag($dtPag);
+        $caixa->setFrPag($frPag);
+        
+        
+       
+       
+        $NrPar = $caixa->getNrPar();
+        $dtPag= $caixa->getDtPag();
+        $frPag= $caixa->getFrPag();
+        
+        
+        
+        
+
+        $DaoCaixa = new DaoCaixa();
+        return $DaoCaixa->inserir($caixa);
+    }
+}
