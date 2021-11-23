5ce45b21ad5f05678a0c54698e1baedf1835cef1
hssuper <higorsouza2306@gmail.com>
2021-11-23 09:05:03 -0300

    add

--- FullCalendar/javascript.js
+++ FullCalendar/javascript.js
@@ -77,7 +77,7 @@ function Calendario() {
           end: "dayGridMonth,timeGridWeek,timeGridDay,listWeek,listMonth",
         },
         eventClick: function (info) {
-          window.location.href = "Consulta.php?id=" + info.event.id;
+          window.location.href = "agendamento.php?id=" + info.event.id;
   
           // change the border color just for fun
           info.el.style.borderColor = "red";
