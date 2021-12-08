d11eca1abaefc543e01bdf0d3bf6611c2ad4dd31
hssuper <higorsouza2306@gmail.com>
2021-12-08 18:01:57 -0300

    add

--- bd/bd.php
+++ bd/bd.php
@@ -5,8 +5,8 @@ class Conecta {
     public function conectadb(){
         $pdo = null;
         try{
-            $pdo = new PDO("mysql:host=localhost;dbname=tattoo", 
-                    "root", "senac");
+            $pdo = new PDO("mysql:host=localhost;dbname=id18087114_tattoo", 
+                    "id18087114_usuariotattoo", "XJ/P|?w/@%k#ze4!");
         } catch (Exception $ex) {
             echo "<script>alert('Erro na conexão com o "
             . "banco de dados.')</script>";
